486d2fb4c5c41beef970799b3b6ab2c5fe87e25f
Don Rouse <Don@HotelDon.com>
2016-03-31 16:29:22 -0700

    Add addUser function
    In an unrelated note, I've decided to start following git best
    practices in terms of commit messages. I'm sure I won't ever
    regret this decision.

--- libs/dblite.js
+++ libs/dblite.js
@@ -2,10 +2,14 @@
 
 var sqlite = require("sqlite3");
 var db = new sqlite.Database("database.db");
+var bcrypt = require("bcrypt");
 
 //SQL statements
 var createUserTable = "CREATE TABLE IF NOT EXISTS users (usr_num INTEGER PRIMARY KEY, username TEXT UNIQUE NOT NULL, password TEXT NOT NULL, email TEXT UNIQUE NOT NULL)";
-var insertNewUser = "INSERT INTO users (username, password) VALUES (?,?)";
+
+var insertNewUser = "INSERT INTO users (username, password, email) VALUES (?,?,?)";
+
+exports.addUser = addUser;
 
 db.serialize(initializeDB);
 
@@ -13,4 +17,35 @@ db.serialize(initializeDB);
 function initializeDB()
 {
     db.run(createUserTable);
+}
+
+function addUser(uname, pword, email, callback)
+{
+    bcrypt.hash(pword, 12, hashResult);
+    
+    function hashResult(error, hash)
+    {    
+        if(!error)
+        {
+            db.run(insertNewUser, uname, hash, email, insertResult);
+        }
+        else
+        {
+            console.log("bcrypt hashing failed? "+ err);
+            callback(false, "bcrypt");
+        }
+        
+        function insertResult(error)
+        {
+            if(!error)
+            {
+                callback(true, "User created successfully.")
+            }
+            else
+            {
+                console.log("Database insert failed! Reason: "+ error);
+                callback(false, "db");
+            }
+        }
+    }
 }
