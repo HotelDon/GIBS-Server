67562b2dbdbdd412c35a8efd1bf88eaf4d63a7fd
Don Rouse <Don@HotelDon.com>
2016-04-01 19:16:49 -0700

    Add stub code.

--- libs/auth.js
+++ libs/auth.js
@@ -1,1 +1,16 @@
 "use strict";
+
+module.exports = function(db){
+    module.login = login;
+    module.register = register;
+    
+    function login()
+    {
+        
+    }
+    
+    function register()
+    {
+        
+    }
+}
