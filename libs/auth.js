3102681bb8e46055c8cb31e00f74e3f7d62c483a
Don Rouse <Don@HotelDon.com>
2016-04-05 18:51:27 -0700

    Add basic reg and login logic
    Current default behavior is for it to log you in immediately
    after registering. Time will tell if this is a good or bad idea.

--- libs/auth.js
+++ libs/auth.js
@@ -4,13 +4,27 @@ module.exports = function(db){
     module.login = login;
     module.register = register;
     
-    function login()
+    function login(uname, pword, callback)
     {
-        
+        db.checkPassword(uname, pword, callback);
     }
     
-    function register()
+    function register(uname, pword, email, callback)
     {
+        db.addUser(uname, pword, email, regCallback);
         
+        function regCallback(err, result)
+        {
+            if(err)
+            {
+                callback(err);
+            }
+            else
+            {
+                login(uname, pword, callback);
+            }
+        }
     }
+    
+    return module;
 }
