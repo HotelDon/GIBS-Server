fdd10bd43030272e80b4a9937796305a52e1fed9
Don Rouse <Don@HotelDon.com>
2016-12-15 20:53:19 -0800

    Add yamlutil.py

---
+++ yaml/yamlutil.py
@@ -0,0 +1,105 @@
+import sys
+
+battlersSchemaFile = "./schemas/battlersSchema.yml"
+movesSchemaFile = "./schemas/movesSchema.yml"
+effectsSchemaFile = "./schemas/effectsSchema.yml"
+
+battlersStartString = "    battlers:\n"
+movesStartString = "    moves:\n"
+effectsStartString = "    effects:\n"
+
+battlersRequiredString = "    - battlers\n"
+movesRequiredString = "    - moves\n"
+effectsRequiredString = "    - effects\n"
+
+battlersSchema = {
+    "file": battlersSchemaFile, 
+    "startString": battlersStartString, 
+    "requiredString": battlersRequiredString
+}
+movesSchema = {
+    "file": movesSchemaFile, 
+    "startString": movesStartString, 
+    "requiredString": movesRequiredString
+}
+effectsSchema = {
+    "file": effectsSchemaFile, 
+    "startString": effectsStartString, 
+    "requiredString": effectsRequiredString
+}
+
+schemas = [battlersSchema, movesSchema, effectsSchema]
+
+endString = "    referenceArrays:\n"
+definitionsString = "definitions:\n"
+
+def main(args):
+
+    if args == "combine":
+        combineSchemas()
+    elif args == "update":
+        updateSchemas()
+    else:
+        print("Invalid Argument. Select from one of the following: combine, update")
+        sys.exit(2)
+        
+def combineSchemas():
+
+    fullText = ""
+    
+    with open("./schemas/systemSchema.yml") as systemSchema:
+        fullText = systemSchema.readlines()
+        
+    for schema in schemas:
+        insertAt = getLineNumber(fullText, endString)
+        fullText.insert(insertAt, getText(schema["file"], schema["startString"], endString))
+        
+        insertAt = getLineNumber(fullText, definitionsString) - 1
+        fullText.insert(insertAt, schema["requiredString"])
+    
+    with open("./schemas/gameSchema.yml", "w") as fullSchema:
+        fullSchema.writelines(fullText)        
+
+def getLineNumber(textList, line):
+    for lineNumber, lineText in enumerate(textList):
+        if (line == lineText):
+            return lineNumber
+
+def getText(fileName, startLine, endLine):
+    with open(fileName) as file:
+        result = ""
+        foundStart = False
+        for line in file:
+            if foundStart:
+                if (line == endLine):
+                    break
+                else:
+                    result = result + line         
+            elif not foundStart and line == startLine:
+                foundStart = True
+                result = result + line     
+        return result
+
+def updateSchemas():
+    definitionText = ""
+    with open("./schemas/systemSchema.yml") as systemSchema:
+        fullText = systemSchema.readlines()
+        lineNumber = getLineNumber(fullText, "definitions:\n")
+        definitionText = fullText[lineNumber:]
+    
+    for schema in schemas:
+        modifiedText = ""
+        with open(schema["file"]) as schemaFile:
+            originalText = schemaFile.readlines()
+            lineNumber = getLineNumber(originalText, "definitions:\n")
+            modifiedText = originalText[0:lineNumber]
+            modifiedText.extend(definitionText)
+            
+        with open(schema["file"], "w") as schemaFile:
+            schemaFile.writelines(modifiedText)
+if __name__ == "__main__":
+    try:
+        main(sys.argv[1]);
+    except IndexError:
+        print ("Missing argument.")
+        sys.exit(2)
