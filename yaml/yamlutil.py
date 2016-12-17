e4fd34dd92fdba56a0f07903e8b9f83916dcf4ed
Don Rouse <Don@HotelDon.com>
2016-12-17 03:11:13 -0800

    Condense yamlutil.py

--- yaml/yamlutil.py
+++ yaml/yamlutil.py
@@ -1,31 +1,19 @@
 import sys
 
-battlersSchemaFile = sys.path[0] + "/schemas/battlersSchema.yml"
-movesSchemaFile = sys.path[0] + "/schemas/movesSchema.yml"
-effectsSchemaFile = sys.path[0] + "/schemas/effectsSchema.yml"
-
-battlersStartString = "    battlers:\n"
-movesStartString = "    moves:\n"
-effectsStartString = "    effects:\n"
-
-battlersRequiredString = "    - battlers\n"
-movesRequiredString = "    - moves\n"
-effectsRequiredString = "    - effects\n"
-
 battlersSchema = {
-    "file": battlersSchemaFile, 
-    "startString": battlersStartString, 
-    "requiredString": battlersRequiredString
+    "file": sys.path[0] + "/schemas/battlersSchema.yml", 
+    "startString": "    battlers:\n", 
+    "requiredString": "    - battlers\n"
 }
 movesSchema = {
-    "file": movesSchemaFile, 
-    "startString": movesStartString, 
-    "requiredString": movesRequiredString
+    "file": sys.path[0] + "/schemas/movesSchema.yml", 
+    "startString": "    moves:\n", 
+    "requiredString": "    - moves\n"
 }
 effectsSchema = {
-    "file": effectsSchemaFile, 
-    "startString": effectsStartString, 
-    "requiredString": effectsRequiredString
+    "file": sys.path[0] + "/schemas/effectsSchema.yml", 
+    "startString": "    effects:\n", 
+    "requiredString": "    - effects\n"
 }
 
 schemas = [battlersSchema, movesSchema, effectsSchema]
@@ -35,14 +23,35 @@ definitionsString = "definitions:\n"
 
 def main(args):
 
-    if args == "combine":
-        combineSchemas()
-    elif args == "update":
+    if args == "updateSchemas":
         updateSchemas()
+    elif args == "combineSchemas":
+        combineSchemas()
+    elif args == "combineExamples":
+        combineExamples()
     else:
         print("Invalid Argument. Select from one of the following: combine, update")
         sys.exit(2)
-        
+
+def updateSchemas():
+
+    definitionText = ""
+    with open(sys.path[0]+"/schemas/systemSchema.yml") as systemSchema:
+        fullText = systemSchema.readlines()
+        lineNumber = getLineNumber(fullText, "definitions:\n")
+        definitionText = fullText[lineNumber:]
+    
+    for schema in schemas:
+        modifiedText = ""
+        with open(schema["file"]) as schemaFile:
+            originalText = schemaFile.readlines()
+            lineNumber = getLineNumber(originalText, "definitions:\n")
+            modifiedText = originalText[0:lineNumber]
+            modifiedText.extend(definitionText)
+            
+        with open(schema["file"], "w") as schemaFile:
+            schemaFile.writelines(modifiedText)
+
 def combineSchemas():
 
     fullText = ""
@@ -79,24 +88,6 @@ def getText(fileName, startLine, endLine):
                 foundStart = True
                 result = result + line     
         return result
-
-def updateSchemas():
-    definitionText = ""
-    with open(sys.path[0]+"/schemas/systemSchema.yml") as systemSchema:
-        fullText = systemSchema.readlines()
-        lineNumber = getLineNumber(fullText, "definitions:\n")
-        definitionText = fullText[lineNumber:]
-    
-    for schema in schemas:
-        modifiedText = ""
-        with open(schema["file"]) as schemaFile:
-            originalText = schemaFile.readlines()
-            lineNumber = getLineNumber(originalText, "definitions:\n")
-            modifiedText = originalText[0:lineNumber]
-            modifiedText.extend(definitionText)
-            
-        with open(schema["file"], "w") as schemaFile:
-            schemaFile.writelines(modifiedText)
 if __name__ == "__main__":
     try:
         main(sys.argv[1]);
